245a983add3ef9373a9391ae5fd53b4c688f903f
Pablo de Oliveira Ferreira <pabloliverfe@gmail.com>
2021-01-18 17:15:00 -0300

    todas as Class do escopo terminadas!

---
+++ exercicioFixHeranca/Entities/ImportedProduct.cs
@@ -0,0 +1,19 @@
+﻿
+
+
+namespace exercicioFixHeranca.Entities
+{
+    class ImportedProduct : Product
+    {
+        public double CustomsFee { get; set; }
+    }
+    public ImportedProduct()
+    {
+
+    }
+
+    public ImportedProduct(string name, double price, double customsFee) : base( name, price)
+    {
+         CustomsFee = customsFee;
+    }
+}
